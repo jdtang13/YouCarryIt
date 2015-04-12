4d331186ec0a70e8cc8ee789e61b2ec4b25fd3be
Jonathan Tang <jdtang13@gmail.com>
2015-04-12 00:20:24 -0400

    Attempt updating jQuery v1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,48 @@
 class WelcomeController < ApplicationController
 
+  def updateSocial
+
+	  require 'twitter'
+
+	  client = Twitter::Streaming::Client.new do |config|
+	    config.consumer_key       = 'QxcbFqox8Zc7ehMUbNFShHZzc'
+	    config.consumer_secret    = 'dOgfPTDm1p8Gn3vKRWiGhjMhawlr9TNeZPpdqNIwx2kHEqoEYQ'
+	    config.access_token        = '2362889286-Rse58rWvw75mbrb9otB92g2eV340TcuWXDHKPpJ'
+	    config.access_token_secret  = 'A8WZ4M56VW1D9b58EgbduXm4QOU0FK7yLiOki3WX1xEza'
+	    #config.auth_method        = :oauth
+	  end  
+
+	  @tweet_limit = 15
+	  count = 0
+
+	  require 'indico'
+	  Indico.api_key = 'a20e64f1e4fd1dd002ec661f62caa444'
+
+	  @statuses = []
+	  @sentiments = []
+
+	 	client.sample do |status| 
+	 		if status.is_a?(Twitter::Tweet) and count < @tweet_limit
+
+	 			if status.text.to_s == ""
+	 				continue
+	 			end
+
+	 			text = (status.text).to_s
+	  			@statuses.push(text)
+	  			@sentiments.push(Indico.sentiment(text))
+	  			count += 1
+	  			#puts count
+	  			#puts status.text.to_s
+	  		else
+	  			break
+	  		end
+	  	end
+
+	  	@tweet_limit = count
+
+  end
+
   def index
 
 	  # require 'twitter'
@@ -12,6 +55,7 @@ class WelcomeController < ApplicationController
 	  #   #config.auth_method        = :oauth
 	  # end  
 
+	  #@tweet_limit = 15
 	  @tweet_limit = 0
 	  # count = 0
 
