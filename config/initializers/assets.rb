213a8e75a08b65cecc11b9630d5f2c35171d872a
Oliver <oliverbarnum32@gmail.com>
2015-04-12 09:29:10 -0400

    Remove footer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,12 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-<<<<<<< HEAD
+
 Rails.application.config.assets.precompile += %w( enemy.js )
 Rails.application.config.assets.precompile += %w( bullet.js )
-=======
 
->>>>>>> origin/master
 Rails.application.config.assets.precompile += %w( cell.js )
 Rails.application.config.assets.precompile += %w( organelle.js )
 Rails.application.config.assets.precompile += %w( canvasgame.js )
