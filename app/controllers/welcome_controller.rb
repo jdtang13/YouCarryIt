1693f9dd8379d58d107505e3a9379af14bb25ebb
Jonathan Tang <jdtang13@gmail.com>
2015-04-12 01:38:21 -0400

    Enable realtime rails-data updating, ~ is delimit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -42,51 +42,54 @@ class WelcomeController < ApplicationController
 
 	  	@tweet_limit = count
 
+	  respond_to do |format|
+	    format.js
+	  end
+
   end
 
   def index
 
-	  # require 'twitter'
+	  require 'twitter'
 
-	  # client = Twitter::Streaming::Client.new do |config|
-	  #   config.consumer_key       = 'QxcbFqox8Zc7ehMUbNFShHZzc'
-	  #   config.consumer_secret    = 'dOgfPTDm1p8Gn3vKRWiGhjMhawlr9TNeZPpdqNIwx2kHEqoEYQ'
-	  #   config.access_token        = '2362889286-Rse58rWvw75mbrb9otB92g2eV340TcuWXDHKPpJ'
-	  #   config.access_token_secret  = 'A8WZ4M56VW1D9b58EgbduXm4QOU0FK7yLiOki3WX1xEza'
-	  #   #config.auth_method        = :oauth
-	  # end  
+	  client = Twitter::Streaming::Client.new do |config|
+	    config.consumer_key       = 'QxcbFqox8Zc7ehMUbNFShHZzc'
+	    config.consumer_secret    = 'dOgfPTDm1p8Gn3vKRWiGhjMhawlr9TNeZPpdqNIwx2kHEqoEYQ'
+	    config.access_token        = '2362889286-Rse58rWvw75mbrb9otB92g2eV340TcuWXDHKPpJ'
+	    config.access_token_secret  = 'A8WZ4M56VW1D9b58EgbduXm4QOU0FK7yLiOki3WX1xEza'
+	    #config.auth_method        = :oauth
+	  end  
 
-	  #@tweet_limit = 15
-	  @tweet_limit = 0
-	  # count = 0
+	  @tweet_limit = 15
+	  count = 0
 
-	  # require 'indico'
-	  # Indico.api_key = 'a20e64f1e4fd1dd002ec661f62caa444'
+	  require 'indico'
+	  Indico.api_key = 'a20e64f1e4fd1dd002ec661f62caa444'
 
-	  # @statuses = []
-	  # @sentiments = []
+	  @statuses = []
+	  @sentiments = []
 
-	 	# client.sample do |status| 
-	 	# 	if status.is_a?(Twitter::Tweet) and count < @tweet_limit
+	 	client.sample do |status| 
+	 		if status.is_a?(Twitter::Tweet) and count < @tweet_limit
 
-	 	# 		if status.text.to_s == ""
-	 	# 			continue
-	 	# 		end
+	 			if status.text.to_s == ""
+	 				continue
+	 			end
 
-	 	# 		text = (status.text).to_s
-	  # 			@statuses.push(text)
-	  # 			@sentiments.push(Indico.sentiment(text))
-	  # 			count += 1
-	  # 			#puts count
-	  # 			#puts status.text.to_s
-	  # 		else
-	  # 			break
-	  # 		end
-	  # 	end
+	 			text = (status.text).to_s
+	  			@statuses.push(text)
+	  			@sentiments.push(Indico.sentiment(text))
+	  			count += 1
+	  			#puts count
+	  			#puts status.text.to_s
+	  		else
+	  			break
+	  		end
+	  	end
 
-	  # 	@tweet_limit = count
+	  	@tweet_limit = count
 
-	  # 	#puts "loop finished"
+	  	#puts "loop finished"
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
-  get '/updateSocial', :controller => :welcome, :action => :updateSocial
+  get 'welcome_controller/updateSocial', :controller => :welcome, :action => :updateSocial
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
